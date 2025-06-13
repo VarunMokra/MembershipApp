1d858a606f3f63f12d2fa2fb9fd2fb7339c57a18
OnlineIDcard <onlineidcard99@gmail.com>
2025-06-13 19:20:49 +0530

    Update Testimonials.jsx

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Banner from "../components/Banner";
 
 const Testimonials = () => {
-  return <Banner imageUrl={"/Testimonails.jpg"} />;
+  return <Banner imageUrl={"/testimonials.jpg"} />;
 };
 
 export default Testimonials;
